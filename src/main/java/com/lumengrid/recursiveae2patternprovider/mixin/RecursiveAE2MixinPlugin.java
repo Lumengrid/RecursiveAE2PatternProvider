4833c34a37ba660404def7d72c48de8955ddb7fd
Joshua Morotti <joshua.morotti@docebo.com>
2025-10-06 08:46:52 +0200

    1.0.5 integrated extendedae assembler

---
+++ src/main/java/com/lumengrid/recursiveae2patternprovider/mixin/RecursiveAE2MixinPlugin.java
@@ -0,0 +1,51 @@
+package com.lumengrid.recursiveae2patternprovider.mixin;
+
+import net.neoforged.fml.loading.LoadingModList;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class RecursiveAE2MixinPlugin implements IMixinConfigPlugin {
+
+    @Override
+    public void onLoad(String mixinPackage) {
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if (mixinClassName.contains("TileAssemblerMatrixPatternMixin")) {
+            boolean isExtendedAELoaded = LoadingModList.get().getModFileById("extendedae") != null;
+            if (!isExtendedAELoaded) {
+                System.out.println("[RecursiveAE2PatternProvider] ExtendedAE not found, skipping TileAssemblerMatrixPatternMixin");
+            }
+            return isExtendedAELoaded;
+        }
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+}
+
